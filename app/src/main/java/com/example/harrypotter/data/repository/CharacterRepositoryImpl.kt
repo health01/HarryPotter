d4916c073f81a15a1d98ba49dc74a4e462d52a89
jason <readonly23@gmail.com>
2025-02-23 04:05:28 +0000

    code tidy up

--- app/src/main/java/com/example/harrypotter/data/repository/CharacterRepositoryImpl.kt
+++ app/src/main/java/com/example/harrypotter/data/repository/CharacterRepositoryImpl.kt
@@ -13,60 +13,53 @@ import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
-
 class CharacterRepositoryImpl @Inject constructor(
     private val harryPotterApiService: HarryPotterApiService,
     private val characterDao: CharacterDao,
-    val characterMapper: CharacterMapper,
-    private val ioDispatcher: CoroutineDispatcher,
-    private val mainDispatcher: CoroutineDispatcher
+    private val characterMapper: CharacterMapper,
+    private val backgroundDispatcher: CoroutineDispatcher, // 更名為 backgroundDispatcher
+    private val uiDispatcher: CoroutineDispatcher // 更名為 uiDispatcher
 ) : CharacterRepository {
+
     override fun getCharacter(): Flow<Results<List<CharacterEntity>>> = flow {
         val response = harryPotterApiService.getCharacterList()
-        val body = response.body()
-        if (response.isSuccessful && body != null) {
-            val detail = characterMapper.mapToUiModel(body)
+        if (response.isSuccessful && response.body() != null) {
+            val detail = characterMapper.mapToUiModel(response.body()!!)
             emit(Results.Success(detail))
         } else {
-            emit(Results.Error("Unable to fetch character details"))
+            val errorMessage = response.errorBody()?.string() ?: "Unable to fetch character details"
+            emit(Results.Error(errorMessage))
         }
     }.catch {
-        emit(Results.Error("Unable to fetch character details"))
-    }.flowOn(mainDispatcher)
-
+        emit(Results.Error("Unable to fetch character details: ${it.message}"))
+    }.flowOn(uiDispatcher)
 
     override fun getCachedCharacterData(): Flow<Results<List<CharacterEntity>>> {
-
-        return characterDao.getAllCharacters().map { result ->
-            if (result.isNotEmpty()) {
-                Results.Success(result)
-            } else {
-                Results.Error("No characters found")
+        return characterDao.getAllCharacters()
+            .map { result ->
+                if (result.isNotEmpty()) Results.Success(result)
+                else Results.Error("No characters found")
             }
-        }
             .catch { e ->
-                emit(Results.Error<List<CharacterEntity>>("Unable to fetch cached data: ${e.message}"))
+                emit(Results.Error("Unable to fetch cached data: ${e.message}"))
             }
-            .flowOn(ioDispatcher)
+            .flowOn(backgroundDispatcher)
     }
 
     override fun searchCachedCharacterData(query: String): Flow<Results<List<CharacterEntity>>> {
         return characterDao.searchCharacters(query)
             .map { result ->
-                if (result.isNotEmpty()) {
-                    Results.Success(result)
-                } else {
-                    Results.Error("No characters found for the query: $query")
-                }
+                if (result.isNotEmpty()) Results.Success(result)
+                else Results.Error("No characters found for the query: $query")
             }
             .catch { e ->
-                emit(Results.Error<List<CharacterEntity>>("Unable to fetch cached data: ${e.message}"))
+                emit(Results.Error("Unable to fetch cached data: ${e.message}"))
             }
-            .flowOn(ioDispatcher)
+            .flowOn(backgroundDispatcher)
     }
 
     override suspend fun updateCachedCharacter(characterEntityList: List<CharacterEntity>) {
-        withContext(ioDispatcher) {
+        withContext(backgroundDispatcher) {
             characterDao.updateCharacters(characterEntityList)
         }
     }
